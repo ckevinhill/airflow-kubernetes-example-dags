3dc6bf0f3878e56346a2f81fab34cb76b4c74ad4
ckevinhill <hill.ck@pg.com>
2020-08-17 21:33:32 +0800

    removed custom operator and changed to pythonoperators

--- test-git-helloword-dag.py
+++ test-git-helloword-dag.py
@@ -1,21 +1,35 @@
 from datetime import datetime
 from airflow import DAG
-from airflow.operators.dummy_operator import DummyOperator
-from airflow.operators.python_operator import PythonOperator
-from airflow.operators import MultiplyBy5Operator
-
-def print_hello():
- return 'Hello Wolrd'
+from airflow.operators.dummy_operator import PythonOperator
 
 dag = DAG('hello_world_via_git', description='Git sync test', schedule_interval='0 12 * * *', start_date=datetime(2017, 3, 20), catchup=False)
 
-dummy_operator = DummyOperator(task_id='dummy_task', retries = 3, dag=dag)
+def print_hello():
+    print ("hello")
 
-hello_operator = PythonOperator(task_id='hello_task', python_callable=print_hello, dag=dag)
+def print_there():
+    print("there")
 
-multiplyby5_operator = MultiplyBy5Operator(my_operator_param='my_operator_param',
-                                task_id='multiplyby5_task', dag=dag)
+def print_world():
+    print("world")
 
-dummy_operator >> hello_operator
+hello_operator = PythonOperator(
+    task_id='hello_task',
+    python_callable=print_hello,
+    dag=dag,
+)
 
-dummy_operator >> multiplyby5_operator
+there_operator = PythonOperator(
+    task_id='there_task',
+    python_callable=print_there,
+    dag=dag,
+)
+
+world_operator = PythonOperator(
+    task_id='world_task',
+    python_callable=print_world,
+    dag=dag,
+)
+
+
+hello_operator >> there_operator >> world_operator
