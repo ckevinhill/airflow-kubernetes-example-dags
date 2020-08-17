d41b8949d7c32e26427399f0d932e78539d2a069
ckevinhill <hill.ck@pg.com>
2020-08-17 21:37:17 +0800

    updated PythonOperator import

--- test-git-helloword-dag.py
+++ test-git-helloword-dag.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from airflow import DAG
-from airflow.operators.dummy_operator import PythonOperator
+from airflow.operators.python_operator import PythonOperator
 
 dag = DAG('hello_world_via_git', description='Git sync test', schedule_interval='0 12 * * *', start_date=datetime(2017, 3, 20), catchup=False)
 
