1c6cf75488316e6c99dbcd7831916ade1b56540e
ckevinhill <hill.ck@pg.com>
2020-08-22 21:18:07 +0800

    renamed and simplified

--- dag-test-python-operator.py
+++ dag-test-python-operator.py
@@ -2,13 +2,15 @@ from datetime import datetime
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 
-dag = DAG('hello_world_via_git', description='Git sync test', schedule_interval='0 12 * * *', start_date=datetime(2017, 3, 20), catchup=False)
+dag = DAG(
+        'test_python_operator', 
+        description='DAG to test PythonOperator', 
+        schedule_interval='@daily', 
+        start_date=datetime(2020, 1, 1), 
+        catchup=False )
 
 def print_hello():
     print ("hello")
-
-def print_there():
-    print("there")
 
 def print_world():
     print("world")
@@ -19,12 +21,6 @@ hello_operator = PythonOperator(
     dag=dag,
 )
 
-there_operator = PythonOperator(
-    task_id='there_task',
-    python_callable=print_there,
-    dag=dag,
-)
-
 world_operator = PythonOperator(
     task_id='world_task',
     python_callable=print_world,
@@ -32,4 +28,4 @@ world_operator = PythonOperator(
 )
 
 
-hello_operator >> there_operator >> world_operator
+hello_operator >> world_operator
